e7d0d5e71cc8c1e9ba94b0a9a4a4cb31ce640c76
Robert Leonard <rleonard@ojolabs.com>
2022-01-07 12:18:32 -0500

    Reading eth client from metamask

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,80 @@
 import React, { Component, useEffect } from "react";
-import getWeb3 from "./getWeb3";
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from "web3";
 
 import "./App.css";
-import Web3 from "web3";
+import { CLIENT_RENEG_WINDOW } from "tls";
+
+type BLOCKCHAIN = "eth" | "fantom" | "avax";
+interface Blockchain {
+  mainnet: number,
+  testnet: number,
+  localnet?: number,
+}
+
+type ChainIds = {
+  [key in BLOCKCHAIN]?: Blockchain;
+};
+
+const CHAIN_IDS: ChainIds = {
+  "eth": {
+    mainnet: 1,
+    testnet: 4
+  },
+}
+
+let metaMaskEthClient;
+let web3Client: Web3;
 
 const App = () => {
 
-  useEffect(() => {
-    loadWeb3()
-  })
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [accounts, setAccounts] = React.useState<string[]>([]);
+  const [chainId, setChainId] = React.useState<number>(-1);
+
+  const loadWeb3 = async () => {
+    try {
+      // Get network provider and web3 instance.
+      metaMaskEthClient = await detectEthereumProvider() as any;
+      //@ts-ignore
+      if (metaMaskEthClient !== window.ethereum) {
+        console.error('Do you have multiple wallets installed?');
+        return;
+      }
+
+      await requestAccountsOnChain();
+      setChainId(await getCurrentChainId());
+
+      web3Client = new Web3(metaMaskEthClient);
+      setIsLoading(false);   
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getCurrentChainId = async (): Promise<number> => {
+    return parseInt( await metaMaskEthClient.request({ method: 'eth_chainId' }));
+  }
+
+  const requestAccountsOnChain = async () => {
+    const accounts = await metaMaskEthClient.request({ method: 'eth_requestAccounts'});
+    console.log(`Accounts are: ${accounts}`);
+    metaMaskEthClient.on('accountsChanged', onAccountsChanged)
+
+
+  }
+
+  const onAccountsChanged = async (accounts: string[]) => {
+    if(accounts.length === 0) {
+      console.log("Please connect to metamask");
+      return;
+    } 
+    setAccounts(accounts);
+  }
+
+  const mainAccount = accounts[0];
+
 
   const minABI = [  
     // balanceOf
@@ -26,25 +92,19 @@ const App = () => {
   
   ];
 
+  useEffect(() => {
+    loadWeb3()
+  })
+
   return (
     <div>
-      <p>Hello World</p> 
+      {
+        isLoading ? <p> Loading </p> : <p> {`Current chain ID: ${chainId}`}</p>
+      }
     </div>
   )
 }
 
-const loadWeb3 = async () => {
-  try {
-    // Get network provider and web3 instance.
-    const web3 = await getWeb3() as Web3;
 
-    // Use web3 to get the user's accounts.
-    const accounts = (await web3.eth.getAccounts()).forEach(async account => console.log(`Account ${account} has a balance of ${await web3.eth.getBalance(account, "latest")}`));
-
-  } catch (error) {
-    // Catch any errors for any of the above operations.
-    console.error(error);
-  }
-};
 
 export default App;
--- client/src/getWeb3.ts
+++ client/src/getWeb3.ts
@@ -4,7 +4,6 @@ const getWeb3 = () =>
   new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
     window.addEventListener("load", async () => {
-      // Modern dapp browsers...
       if (window.ethereum) {
         const web3 = new Web3(window.ethereum);
         try {
@@ -17,12 +16,6 @@ const getWeb3 = () =>
         }
       }
       // Legacy dapp browsers...
-      else if (window.web3) {
-        // Use Mist/MetaMask's provider.
-        const web3 = window.web3;
-        console.log("Injected web3 detected.");
-        resolve(web3);
-      }
       // Fallback to localhost; use dev console port by default...
       else {
         const provider = new Web3.providers.HttpProvider(
